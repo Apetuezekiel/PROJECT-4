a35d9d718b76775c90a660fad4321dac5cc3fe12
Apetuezekiel <62860477+Apetuezekiel@users.noreply.github.com>
2020-04-22 21:16:03 +0100

    Create project.js

---
+++ project.js
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DIGITAL CLOCK</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/home/apetu/Desktop/EZEKIEL/CSS/project7.css">
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="main-containert">
+        <h1>DIGITAL CLOCK</h1>
+        <div class="container">
+            <div class="hrContainer section">
+                <div id="hour" class="ctr">0</div>
+                <div id="hourStr" class="ctr">hr</div>
+            </div>
+            <div class="minContainer section">
+                <div id="min" class="ctr">0</div>
+                <div id="minStr" class="ctr">min</div>
+            </div>
+            <div class="secContainer section">
+                <div id="sec" class="ctr">0</div>
+                <div id="secStr" class="ctr">secs</div>
+            </div>
+        </div>
+    </div>
+    <script src="/home/apetu/Desktop/EZEKIEL/JS/project7.js"></script>
+</body>
+</html>
